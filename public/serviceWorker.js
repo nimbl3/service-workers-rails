3aa997a6c88d9780e767251b864d8a0aea4ed3d3
abhinav <makeawish880@gmail.com>
2017-03-03 15:59:32 +0700

    users listing and profile page added with some improvements in service worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -15,29 +15,28 @@ self.addEventListener('install', function (event) {
         console.log('[ServiceWorker] Caching app shell');
         return cache.addAll(urlsToCache);
       })
-    .catch(function() {
-      console.error('cache failure')
-    })
   );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-    .catch(function() {
-      debugger
-    })
-    .then(function (response) {
+      .then(function(response) {
         // Cache hit - return response
         if (response) {
           return response;
         }
         
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
         var fetchRequest = event.request.clone();
-        return fetch(fetchRequest)
-          .then(function (response) {
+        
+        return fetch(fetchRequest).then(
+          function(response) {
             // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
             
@@ -48,22 +47,13 @@ self.addEventListener('fetch', function (event) {
             var responseToCache = response.clone();
             
             caches.open(CACHE_NAME)
-              .then(function (cache) {
+              .then(function(cache) {
                 cache.put(event.request, responseToCache);
-              })
-              .catch(function(error) {
-                debugger
-                return {
-                  error: error.message
-                };
               });
+            
             return response;
-          })
-          .catch(function(error) {
-            return {
-              error: error.message
-            };
-          });
+          }
+        );
       })
   );
 });
